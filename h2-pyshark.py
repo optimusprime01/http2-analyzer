bd73c924c3124ba855a0eec646b119be6761f210
Sachin Sunder Sagadevan <sachsun2809@outlook.com>
2021-01-17 11:39:26 -0800

    adding first example for http2 parsing

--- h2-pyshark.py
+++ h2-pyshark.py
@@ -5,19 +5,52 @@ import sys
 import pyshark
 from h2.config import H2Configuration
 from h2.connection import H2Connection
+from h2.frame_buffer import FrameBuffer
+from h2.frame_buffer import HeadersFrame
+from hyperframe.frame import *
+from hpack import Decoder
 
-# Change FILENAME to your pcap file's name.
-FILENAME = "./pcap-sample/http2-h2c.pcap"
+decoder = Decoder()
+
+# Change INPUT_CONFIG to your pcap file's name and server/client's IP address
+INPUT_CONFIG = {
+    "pcap": "./pcap-sample/http2-h2c.pcap",
+    "client": "10.9.0.2",
+    "server": "139.162.123.134",
+}
 # Change STREAM_NUMBER to the stream number you want to follow.
 STREAM_NUMBER = 0
+# Switching protocol check
+SWITCHING_PRTOCOL_TXT = "HTTP/1.1 101 Switching Protocols"
 
 # open the pcap file, filtered for a single TCP stream
-cap = pyshark.FileCapture(FILENAME)
+cap = pyshark.FileCapture(INPUT_CONFIG["pcap"])
 
-# sys.exit()
+sess_index = set()
+curr_pkt = cap.next()
+while curr_pkt:
+    try:
+        sess_index.add(curr_pkt.tcp.stream)
+        curr_pkt = cap.next()
+    except StopIteration:
+        break
+    except:
+        pass
+print(sess_index)
+cap.reset()
 
-config = H2Configuration(client_side=False)
-conn = H2Connection(config=config)
+# server_config = H2Configuration(client_side=False)
+# client_config = H2Configuration(client_side=True)
+# server = H2Connection(config=server_config)
+# client = H2Connection(config=client_config)
+server_buffer = FrameBuffer(server=True)
+server_buffer.max_frame_size = 262144
+client_buffer = FrameBuffer(server=False)
+client_buffer.max_frame_size = 262144
+
+client_events = []
+server_events = []
+
 
 curr_pkt = cap.next()
 while curr_pkt:
@@ -27,13 +60,47 @@ while curr_pkt:
             tmp_data = curr_pkt.tcp.payload.encode()
             h_list = [e for e in tmp_data.decode("utf-8").split(":")]
             tdata = "".join(h_list)
-            byte_data = (
-                bytes.fromhex(tdata)
-                .decode("utf-8")
-                .replace("HTTP/1.1 101 Switching Protocols\r\nConnection: Upgrade\r\nUpgrade: h2c\r\n\r\n", "")
-            )
-            print(byte_data)
-            conn.receive_data(byte_data)
+            byte_data = bytes.fromhex(tdata)
+            try:
+                byte_data_str = byte_data.decode("utf-8")
+            except:
+                byte_data_str = ""
+            if SWITCHING_PRTOCOL_TXT in byte_data_str and curr_pkt.ip.dst == INPUT_CONFIG["client"]:
+                # client.initiate_upgrade_connection()
+                # print(client.data_to_send())
+                curr_pkt = cap.next()
+                continue
+            # print([curr_pkt.ip.dst, byte_data], type(byte_data))
+            if curr_pkt.ip.dst == INPUT_CONFIG["client"]:
+                # events = client.receive_data(byte_data)
+                # print(events)
+                client_buffer.add_data(byte_data)
+                # print(client_buffer.data)
+                for event in client_buffer:
+                    # print("Client event: {0}".format(event))
+                    client_events.append(event)
+            elif curr_pkt.ip.dst == INPUT_CONFIG["server"]:
+                # print("here")
+                # events = server.receive_data(byte_data)
+                # print(events)
+                server_buffer.add_data(byte_data)
+                for event in server_buffer:
+                    # print("Server event: {0}".format(event))
+                    server_events.append(event)
         curr_pkt = cap.next()
     except StopIteration:
         break
+
+for event in client_events:
+    print("Client event: {0}, streamid: {1}".format(event, event.stream_id))
+    if isinstance(event, HeadersFrame):
+        print("Client headers: {0}".format(decoder.decode(event.data)))
+    if isinstance(event, DataFrame):
+        print("Client received data: {0}".format(event.data.decode("utf-8")))
+
+for event in client_events:
+    print("Server event: {0}, streamid: {1}".format(event, event.stream_id))
+    if isinstance(event, HeadersFrame):
+        print("Server headers: {0}".format(decoder.decode(event.data)))
+    if isinstance(event, DataFrame):
+        print("Server received data: {0}".format(event.data.decode("utf-8")))
